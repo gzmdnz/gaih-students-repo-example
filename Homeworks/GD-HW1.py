680c8856ff3dbe005bc0ffe62f91c6d860be50ef
gzmdnz <denizgizem91@gmail.com>
2021-03-10 23:26:18 +0300

    Add files via upload

---
+++ Homeworks/GD-HW1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+list1=[2,4,6,8,10]
+list2=[1,3,5,7,9]
+newlist=list1+list2
+print(newlist)
+
+
+# In[12]:
+
+
+carpım=[]
+
+for x in newlist:
+    carpım.append(2*x)
+print(carpım)
+
+
+# In[14]:
+
+
+liste=sorted(carpım)
+print(liste)
+
+
+# In[15]:
+
+
+for x in liste:
+    print(type(x))
+
+
+
+
+# In[ ]:
+
+
+
+
